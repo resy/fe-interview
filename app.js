8dc976cd14b931bfd20439e237a32b2c2e5dcfa7
SARAH SHOWERS <sshowers@linkedin.com>
2021-08-06 15:02:33 -0400

    populate dropdown with data

--- app.js
+++ app.js
@@ -6,10 +6,21 @@
   fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
-      let breeds = data.message;
-      debugger;
+      return populateDropdownList(data.message);
     })
     .catch(function (error) {
       console.log(error);
     });
 })();
+
+let populateDropdownList = (breeds) => {
+  const selectEl = document.getElementById('breed-selector');
+
+  for (const key in breeds) {
+    const optionEl = document.createElement('option');
+    optionEl.value = key;
+    optionEl.text = key;
+    selectEl.add(optionEl);
+  }
+  return;
+};
